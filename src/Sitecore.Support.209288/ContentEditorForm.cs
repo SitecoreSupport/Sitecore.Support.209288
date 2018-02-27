c347af83abe5e6d0df3a6f432fa688a862256a99
DK\pk <pk@sitecore.net>
2018-02-27 17:26:55 +0200

    Does not show context menu on validation bar is security is denied

---
+++ src/Sitecore.Support.209288/ContentEditorForm.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Security.AccessControl;
+
+namespace Sitecore.Support.Shell.Applications.ContentManager
+{
+    public class ContentEditorForm : Sitecore.Shell.Applications.ContentManager.ContentEditorForm
+    {
+        protected new void Validator_ContextMenu(string markerId)
+        {
+            var field = Context.ContentDatabase.GetItem("{F47C0D78-61F9-479C-96DF-1159727D32C6}");
+            if (AuthorizationManager.IsAllowed(field, AccessRight.FieldWrite, Context.User))
+            {
+                base.Validator_ContextMenu(markerId);
+            }
+        }
+    }
+}
